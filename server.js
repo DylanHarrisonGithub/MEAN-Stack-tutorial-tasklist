fa4aefae17764ef3c222b8f4dded62630fd0ecec
Dylan Harrison <dillysnaps@gmail.com>
2017-12-07 18:23:23 -0600

    routes-setup

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
+var cors = require('cors');
 
  var index = require('./routes/index');
  var tasks = require('./routes/tasks');
@@ -8,7 +9,8 @@ var bodyParser = require('body-parser');
  var port = 3000;
 
  var app = express();
-
+ app.use(cors())
+ 
  //view engine
  app.set('views', path.join(__dirname, 'views'));
  app.set('view engine', 'ejs');
